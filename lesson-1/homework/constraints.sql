b3ea10d12497b9d3dba86bcedfd32aec948b8fff
diyoravlv <diyora527@gmail.com>
2025-02-25 22:11:09 +0500

    Update constraints.sql

--- lesson-1/homework/constraints.sql
+++ lesson-1/homework/constraints.sql
@@ -1,1 +1,14 @@
+create database HomeworkDB
+use HomeworkDB
+create table Students (StudentId int, FullName varchar(50), Age int, GPA decimal (3,2))
+Alter table Students 
+add Email varchar(50) 
+sp_rename 'Students.FullName', 'Name', 'COLUMN'
+Alter table Students 
+drop column Age
+truncate table Students 
 
+
+Select @@SERVERNAME
+Select GETDATE()
+Select * from Students
